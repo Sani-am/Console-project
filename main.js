4ba13d449546ec1cbc4cad13ea2b064ea7359565
Sani-am <79805957+Sani-am@users.noreply.github.com>
2021-08-06 23:06:54 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+var fname= "sani";
+var lname= "amin";
+var fullname= (fname).concat(lname);
+var length_of_name=fullname.length;
+var a=55;
+var b=10;
+var add= a+b;
+var sub= a-b;
+
+
+
+
+console.log(fname);
+console.log(lname);
+console.log(fullname);
+console.log(length_of_name);
+console.log(a);
+console.log(b);
+console.log(add);
+console.log(sub);
+console.log("THIS IS MY ADV-77 PROJECT");
